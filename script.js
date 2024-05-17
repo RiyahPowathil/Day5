ff6d96b418fad72889ceca9eb8a49d91bafffe6a
RiyahPowathil <riyapowathil@gmail.com>
2024-05-17 10:57:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function matchpass(){
+    var firstpass=document.f1.password.value;
+    var secondpass=document.f1.password2.value;
+    if(firstpass == secondpass){
+        alert("your password is correct😶😶😶😶");
+        return true;
+    }
+    else{
+        alert("password must be same!!🤯🤯🤯")
+        return false;
+    }
+}
